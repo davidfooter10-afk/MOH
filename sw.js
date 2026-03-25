0f3fbbb08126db61cfaa62f388cd25dd83f02760
davidfooter10-afk <davidfooter10@gmail.com>
2026-03-25 18:49:30 +0300

    Delete sw.js

--- sw.js
+++
@@ -1,6 +0,0 @@
-self.addEventListener('install', (event) => {
-  console.log('Service worker installed');
-});
-
-// This empty fetch listener is required by mobile browsers to show the "Download App" prompt
-self.addEventListener('fetch', (event) => {});
